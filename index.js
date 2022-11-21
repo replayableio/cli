57d14ac61686641c2cf3d1f4b150168c6c17a35a
Ian Jennings <ian@meetjennings.com>
2022-11-21 15:30:17 -0600

    drop uneeded config vars

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 #!/usr/bin/env node
 
-const lib = require('./lib')
-const { program } = require('commander');
+const lib = require("./lib");
+const { program } = require("commander");
 
-let stdin = '';
+let stdin = "";
 
-program.name('replayable')
-  .description('Upgrade your bug reports, pulls, and readmes with clips from local development')
-  .version('0.0.9');
+program
+  .name("replayable")
+  .description(
+    "Upgrade your bug reports, pulls, and readmes with clips from local development"
+  )
+  .version("0.0.9");
 
 program.showHelpAfterError();
 
-program.command('create', { isDefault: true })
-  .description('Create a replay and output the resulting url or markdown. Will launch desktop app for local editing before publishing.')
-  .option('-t, --title <string>', 'Title of the replay. Automatically generated if not supplied.')
-  .option('-d, --description [text]', 'Replay markdown body. This may be piped in.')
-  .option('-p, --private', 'Do not expose secret share key in response.')
-  .option('--link', 'Return a link instead of markdown.')
-  .option('--png', 'Return a static png instead of gif. Only applies if -l is not supplied.')
+program
+  .command("create", { isDefault: true })
+  .description(
+    "Create a replay and output the resulting url or markdown. Will launch desktop app for local editing before publishing."
+  )
+  .option(
+    "-t, --title <string>",
+    "Title of the replay. Automatically generated if not supplied."
+  )
+  .option(
+    "-d, --description [text]",
+    "Replay markdown body. This may be piped in."
+  )
+  .option("--md", "Returns a rich markdown link.")
   .action(async function (str, options) {
-
     try {
-
       let description = this.opts().description;
       if (stdin) {
         description = stdin;
@@ -31,36 +39,32 @@ program.command('create', { isDefault: true })
         title: this.opts().title,
         description,
         private: this.opts().private,
-        link: this.opts().link,
-        png: this.opts().png
+        md: this.opts().md,
+        png: this.opts().png,
       });
       console.log(result);
     } catch (e) {
-      console.log('Error: ', e)
+      console.log("Error: ", e);
     }
     process.exit(0);
-
   });
-
 
 if (process.stdin.isTTY) {
   program.parse(process.argv);
-}
-else {
-  process.stdin.on('error', function () { });
-  process.stdin.on('readable', function () {
-
+} else {
+  process.stdin.on("error", function () {});
+  process.stdin.on("readable", function () {
     var chunk = this.read();
 
     if (chunk !== null) {
       stdin += chunk;
     }
   });
-  process.stdin.on('end', function () {
+  process.stdin.on("end", function () {
     program.parse(process.argv);
   });
 }
 
 if (module.parent) {
-  module.exports = lib
+  module.exports = lib;
 }
--- lib.js
+++ lib.js
@@ -7,91 +7,53 @@ ipc.config.maxRetries = 0;
 
 const connectToIpc = function () {
   return new Promise((resolve, reject) => {
-
     ipc.connectTo("replayable");
-    ipc.of.replayable.on("connect", resolve)
+    ipc.of.replayable.on("connect", resolve);
     ipc.of.replayable.on("error", (e) => {
-
-      if (e.code === 'ENOENT') {
-        console.log('Could not connect to Replayable Desktop App. Is it running?')
-        console.log('You may need to download and install the app from https://bit.ly/3erITXJ')
+      if (e.code === "ENOENT") {
+        console.log(
+          "Could not connect to Replayable Desktop App. Is it running?"
+        );
+        console.log(
+          "You may need to download and install the app from https://bit.ly/3erITXJ"
+        );
       }
-    })
+    });
     ipc.of.replayable.on("disconnect", function () {
-      console.log("disconnected from replayable".notice);
+      console.log("Disconnected from replayable".notice);
     });
   });
-}
+};
 
 const createReplay = async function (options = {}) {
+  options.md = options.md || false;
 
-  options.private = options.private || false;
-  options.png = options.png || false;
-  options.link = options.link || false;
   options.title = options.title || false;
   options.description = options.description || null;
 
   await connectToIpc();
 
-  const pngUrl = function (data) {
-    let url = `${data.API_ENDPOINT}/replay/${
-      data.replay.id
-      }/screenshot${shareQuery(data)}`;
-    return url;
-  };
-
-  const gifUrl = function (data) {
-    let url = `${data.API_ENDPOINT}/replay/${data.replay.id}/gif${shareQuery(
-      data
-    )}`;
-    return url;
-  };
-
-  const imageUrl = function (data) {
-    if (options.png) {
-      return pngUrl(data);
-    } else {
-      return gifUrl(data);
-    }
-  };
-
-  const shareLink = function (data) {
-    return `${data.WEB_URL}/replay/${data.replay.id}/${shareQuery(data)}`;
-  };
-
-  const shareQuery = function (data) {
-    return options.private ? "" : `?share=${data.replay.shareKey}`;
-  };
-
-  const markdownPreview = function (data) {
-    return `[![${data.replay.title}](${imageUrl(data)})](${shareLink(data)})
-
-Watch [${data.replay.title}](${shareLink(data)}) on Replayable`;
-  };
-
   return new Promise(async (resolve, reject) => {
-
     ipc.of.replayable.on(
       "upload", //any event or message type your server listens for
       function (data) {
-        // this is essentially the return value, as this echos to cli
-        if (options.link) {
-          resolve(shareLink(data));
+        if (options.md) {
+          resolve(data.replay.markdown);
         } else {
-          resolve(markdownPreview(data));
+          resolve(data.replay.shareLink);
         }
       }
     );
 
-    console.log('emitting create', options.description)
+    setTimeout(() => {
+      reject("timeout");
+    }, 60000);
 
     ipc.of.replayable.emit("create", {
       title: options.title,
-      description: options.description
+      description: options.description,
     });
-
   });
-
 };
 
 module.exports = { createReplay };
